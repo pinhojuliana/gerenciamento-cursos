57512772373345198845040ffac2394c0f25b11b
pinhojuliana <studyjulianaa@gmail.com>
2024-09-10 16:26:31 -0300

    cria a classe Manage, Student e Teacher com seus respectivos construtores e faz a configuração inicial da classe Main

--- src/main/java/com/juliana/gerenciamento_cursos/Application/Main.java
+++ src/main/java/com/juliana/gerenciamento_cursos/Application/Main.java
@@ -1,4 +1,36 @@
 package com.juliana.gerenciamento_cursos.Application;
 
+import com.juliana.gerenciamento_cursos.entity.educational_platform.EducationalPlatform;
+
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        EducationalPlatform educationalPlatform = new EducationalPlatform();
+
+        System.out.println("Bem-vindo ao sistema de Cursos");
+        System.out.println("Primeiro informe sua identidade ou encerre o programa escolhendo a opção 'sair':");
+        System.out.println("1-Administrador\n2-Professor\n3-Aluno\n4-sair");
+        int option = scanner.nextInt();
+
+                switch (option){
+                    case 1:
+                        Manager manager = new Manager(educationalPlatform, scanner);
+                        break;
+                    case 2:
+                        Teacher teacher = new Teacher(educationalPlatform, scanner);
+                        break;
+                    case 3:
+                        Student student = new Student(educationalPlatform, scanner);
+                        break;
+                    case 4:
+                        return;
+                    default:
+                        System.out.println("Opção inválida");
+                }
+
+
+    }
 }
---
+++ src/main/java/com/juliana/gerenciamento_cursos/Application/Teacher.java
@@ -0,0 +1,14 @@
+package com.juliana.gerenciamento_cursos.Application;
+
+import com.juliana.gerenciamento_cursos.entity.educational_platform.EducationalPlatform;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Scanner;
+
+@Data
+@AllArgsConstructor
+public class Teacher {
+    private EducationalPlatform educationalPlatform;
+    private Scanner scanner;
+}
