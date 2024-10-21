da1ffaf095b98fb8c627c74edebac41cab2667db
pinhojuliana <studyjulianaa@gmail.com>
2024-10-21 17:29:04 -0300

    cria interfaces repository

---
+++ src/main/java/com/juliana/gerenciamento_cursos/entity/course/CourseController.java
@@ -0,0 +1,9 @@
+package com.juliana.gerenciamento_cursos.entity.course;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/courses")
+public class CourseController {
+}
---
+++ src/main/java/com/juliana/gerenciamento_cursos/entity/module_section/ModuleSectionController.java
@@ -0,0 +1,9 @@
+package com.juliana.gerenciamento_cursos.entity.module_section;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/modules")
+public class ModuleSectionController {
+}
