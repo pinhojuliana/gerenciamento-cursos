aab3b965f9315ff2fa74f506b9b0d60394cf6baf
pinhojuliana <studyjulianaa@gmail.com>
2024-10-26 15:08:25 -0300

    cria excessoes para validar a unicidade dos titulos dos cursos e para senhas que não correspondem com as presentes no db

--- src/main/java/com/juliana/gerenciamento_cursos/exceptions/InvalidPasswordException.java
+++ src/main/java/com/juliana/gerenciamento_cursos/exceptions/InvalidPasswordException.java
@@ -1,7 +1,7 @@
 package com.juliana.gerenciamento_cursos.exceptions;
 
 public class InvalidPasswordException extends RuntimeException {
-  public InvalidPasswordException(String message) {
-    super(message);
-  }
+    public InvalidPasswordException(String message) {
+        super(message);
+    }
 }
