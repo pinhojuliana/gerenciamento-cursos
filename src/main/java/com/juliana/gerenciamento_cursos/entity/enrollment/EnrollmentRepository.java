433d9138a189bb18252fc16ede5aa5e2f9cd31f2
pinhojuliana <studyjulianaa@gmail.com>
2024-10-26 13:44:07 -0300

    Implementa metodos classe Enrollment

--- src/main/java/com/juliana/gerenciamento_cursos/entity/enrollment/EnrollmentRepository.java
+++ src/main/java/com/juliana/gerenciamento_cursos/entity/enrollment/EnrollmentRepository.java
@@ -2,7 +2,13 @@ package com.juliana.gerenciamento_cursos.entity.enrollment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface EnrollmentRepository extends JpaRepository<Enrollment, UUID> {
+    List<Enrollment> findByStudentIdList(UUID studentId);
+    List<Enrollment> findByCourseId(UUID courseId);
+
+    Optional<Enrollment> findByStudentId(UUID studentId);
 }
--- src/main/java/com/juliana/gerenciamento_cursos/entity/enrollment/EnrollmentService.java
+++ src/main/java/com/juliana/gerenciamento_cursos/entity/enrollment/EnrollmentService.java
@@ -2,39 +2,46 @@ package com.juliana.gerenciamento_cursos.entity.enrollment;
 
 import com.juliana.gerenciamento_cursos.entity.course.Course;
 import com.juliana.gerenciamento_cursos.entity.user.student.Student;
+import com.juliana.gerenciamento_cursos.exceptions.InexistentOptionException;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.UUID;
 
 @NoArgsConstructor
 public class EnrollmentService {
     @Autowired
-    EnrollmentRepository enrollmentRepository;
+    EnrollmentRepository repository;
 
-    public void showEnrollments(Course course){
-
+    public List<Enrollment> showEnrollmentsCourse(UUID courseId){
+        return repository.findByCourseId(courseId);
     }
 
-    public void showEnrollments(Student student){
-
+    public List<Enrollment> showEnrollmentsStudent(UUID studentId){
+        return repository.findByStudentIdList(studentId);
     }
 
-    public void showAllEnrollments(){
-
+    public List<Enrollment> showAllEnrollments(){
+        return repository.findAll();
     }
 
+    //checar como vou receber isso -> UUID?
     public EnrollmentResponse enrollStudentInCourse(EnrollmentRequestPayload enrollmentRequestPayload){
         Enrollment newEnrollment = new Enrollment(enrollmentRequestPayload.course(), enrollmentRequestPayload.student());
-        enrollmentRepository.save(newEnrollment);
+        repository.save(newEnrollment);
 
         return new EnrollmentResponse(newEnrollment.getId());
     }
 
-    //criar condição que quando uma inscrição é colocada como inativa
-    //ela nao pode ser colocada como ativa novamente
-    public void unsubscribleStudentOfCourse(Student student, Course course){
-
+    public void unsubscribleStudentOfCourse(UUID studentId, UUID courseId) throws InexistentOptionException {
+      Enrollment enrollment = repository.findByStudentId(studentId).orElseThrow(() -> new InexistentOptionException("Usuario não encontrado"));
+        if (enrollment.getCourse().getId() == courseId){
+            enrollment.setActive(false);
+            repository.save(enrollment);
+        } else {
+            throw new InexistentOptionException("Inscrição ou curso inexistente");
+        }
     }
-
-    //criar metodo para desativar automaticamente apos a data
 
 }
