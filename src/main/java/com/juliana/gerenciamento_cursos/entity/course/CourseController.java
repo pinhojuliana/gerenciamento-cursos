fe06a5b717ef9de3f07b1203272e73ddb6908c59
pinhojuliana <studyjulianaa@gmail.com>
2024-10-30 14:10:41 -0300

    Cria metodos da classe CourseController

--- src/main/java/com/juliana/gerenciamento_cursos/entity/course/CourseController.java
+++ src/main/java/com/juliana/gerenciamento_cursos/entity/course/CourseController.java
@@ -1,9 +1,38 @@
 package com.juliana.gerenciamento_cursos.entity.course;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/courses")
 public class CourseController {
+    @Autowired
+    CourseService service;
+
+    @GetMapping
+    public ResponseEntity<List<Course>> showCourses(){
+        List<Course> courses = service.showCourses();
+        return ResponseEntity.ok(courses);
+    }
+
+    @PostMapping("/register")
+    public CourseResponse createNewCourse(@RequestBody CourseRequestPayload requestPayload){
+        return service.createNewCourse(requestPayload);
+    }
+
+    @PutMapping("/{id}/description")
+    public ResponseEntity<String> alterDescription(@PathVariable UUID id, @RequestBody String description){
+        service.alterDescription(id, description);
+        return ResponseEntity.ok("Descrição atualizada com sucesso");
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteCourse(@PathVariable UUID id){
+        service.deleteCourse(id);
+        return ResponseEntity.ok("Curso apagado com sucesso");
+    }
 }
