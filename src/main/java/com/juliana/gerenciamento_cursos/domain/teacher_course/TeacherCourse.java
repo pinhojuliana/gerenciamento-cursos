ffddfe446a7f42d788e26e70bc2bcbb3db024515
pinhojuliana <studyjulianaa@gmail.com>
2024-11-05 16:51:06 -0300

    cria entidade TeacherCourse

--- src/main/java/com/juliana/gerenciamento_cursos/domain/teacher_course/TeacherCourse.java
+++ src/main/java/com/juliana/gerenciamento_cursos/domain/teacher_course/TeacherCourse.java
@@ -1,4 +1,23 @@
 package com.juliana.gerenciamento_cursos.domain.teacher_course;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "teacher_course")
+@Getter
+@Setter
 public class TeacherCourse {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(name = "teacher_id")
+    private UUID teacherId;
+
+    @Column(name = "course_id")
+    private UUID courseId;
 }
