db3207dcb84571dde1a5de16c528cf1328fda10b
pinhojuliana <studyjulianaa@gmail.com>
2024-11-11 14:49:36 -0300

    feat: adiciona métodos de pesquisa com EntityGraph

--- src/main/java/com/juliana/gerenciamento_cursos/repository/CourseRepository.java
+++ src/main/java/com/juliana/gerenciamento_cursos/repository/CourseRepository.java
@@ -1,12 +1,18 @@
 package com.juliana.gerenciamento_cursos.repository;
 
 import com.juliana.gerenciamento_cursos.domain.course.Course;
+import io.micrometer.common.lang.NonNullApi;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@NonNullApi
 public interface CourseRepository extends JpaRepository<Course, UUID> {
     Optional<Course> findByTitle(String title);
     boolean existsByTitle(String title);
+
+    @EntityGraph(attributePaths = "teachers")
+    Optional<Course> findById(UUID id);
 }
--- src/main/java/com/juliana/gerenciamento_cursos/repository/TeacherRepository.java
+++ src/main/java/com/juliana/gerenciamento_cursos/repository/TeacherRepository.java
@@ -1,16 +1,21 @@
 package com.juliana.gerenciamento_cursos.repository;
 
 import com.juliana.gerenciamento_cursos.domain.client.Teacher;
+import io.micrometer.common.lang.NonNullApi;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@NonNullApi
 public interface TeacherRepository extends JpaRepository<Teacher, UUID> {
     List<Teacher> findByName(String name);
 
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
-    boolean existsSkill(UUID teacherId, String skill);
+
+    @EntityGraph(attributePaths = "coursesTaught")
+    Optional<Teacher> findById(UUID id);
 }
