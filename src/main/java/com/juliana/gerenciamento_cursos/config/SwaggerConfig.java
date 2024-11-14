2d19e27d4336341ba33781feee2fdd1e0692dda8
pinhojuliana <studyjulianaa@gmail.com>
2024-11-14 11:17:49 -0300

    refactor: organiza pacote resources

--- src/main/java/com/juliana/gerenciamento_cursos/config/SwaggerConfig.java
+++ src/main/java/com/juliana/gerenciamento_cursos/config/SwaggerConfig.java
@@ -1,4 +1,17 @@
 package com.juliana.gerenciamento_cursos.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .packagesToScan("com.juliana.gerenciamento_cursos.controller")
+                .build();
+    }
 }
