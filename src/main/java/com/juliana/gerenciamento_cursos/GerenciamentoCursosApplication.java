e224f4215df7b533bb2eb56790514210b1862ccb
pinhojuliana <studyjulianaa@gmail.com>
2024-11-15 11:43:19 -0300

    bugfix: corrigeerro de cors

--- src/main/java/com/juliana/gerenciamento_cursos/GerenciamentoCursosApplication.java
+++ src/main/java/com/juliana/gerenciamento_cursos/GerenciamentoCursosApplication.java
@@ -1,8 +1,11 @@
 package com.juliana.gerenciamento_cursos;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "default Server URL")})
 @SpringBootApplication
 public class GerenciamentoCursosApplication {
 
