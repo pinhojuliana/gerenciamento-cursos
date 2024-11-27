9096ee31bdd44232268ee1ddd4456a3f3c75d717
pinhojuliana <studyjulianaa@gmail.com>
2024-11-27 16:13:00 -0300

    feat: implementa filtros na classe de configuração

--- src/main/java/com/juliana/gerenciamento_cursos/security/SecurityConfig.java
+++ src/main/java/com/juliana/gerenciamento_cursos/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.juliana.gerenciamento_cursos.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,16 +8,26 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final SecurityStudentFilter securityStudentFilter;
+
+    private final SecurityTeacherFilter securityTeacherFilter
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> {
                    auth.requestMatchers("/courses").permitAll();
                     auth.anyRequest().authenticated();
-                });
+                })
+                .addFilterBefore(securityStudentFilter, BasicAuthenticationFilter.class)
+                .addFilterBefore(securityTeacherFilter, BasicAuthenticationFilter.class)
+        ;
 
         return http.build();
     }
